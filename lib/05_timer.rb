080d979078f4c998d32335d2ee191c1a09b82527
nirina <RDavidNH>
2019-10-08 11:33:28 +0300

    updated 05_timer

--- lib/05_timer.rb
+++ lib/05_timer.rb
@@ -1,12 +1,22 @@
-def time_string(duration)
-    
-    if duration == 0    
-        return "00:00:00"
-    elsif duration < 60
-        return "00:00:#{duration}"
-    elsif duration > 60
-        
-    end
-
+def time_string(secs)
+  h = secs / 3600;  # hours
+  m = secs % 3600 / 60; # minutes
+  s = secs % 60; #seconds
   
+  # add 0 before each number less than 10 to have the format 01:01:01
+  if h < 10
+    h = "0" + h.to_s
+  end
+  
+  if m < 10
+    m = "0" + m.to_s
+  end
+  
+  if s < 10
+    s = "0" + s.to_s
+  end
+  
+  return "#{h}:#{m}:#{s}"
 end
+
+
